f5c4835d9319be9617f8789a2fd5ed874deaeefe
raphaelanjos1 <raphael-anjos@live.com>
2019-06-11 00:25:55 -0300

    add desafio-01 folder

---
+++ DOM/desafio-01/index.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#desafio',
+  data: {
+    name: 'Raphael',
+    age: 23,
+    src: 'https://i.pinimg.com/originals/2c/78/bb/2c78bb1fd4b3d87391615cc22c1dd57f.webp'
+  },
+  methods: {
+    randomNumber() {
+      return Math.random()
+    }
+  }
+})
