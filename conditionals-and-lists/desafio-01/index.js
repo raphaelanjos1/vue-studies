c46910efaca94ab657ecd5849c27e586624c3353
raphaelanjos1 <raphael-anjos@live.com>
2019-06-27 18:14:14 -0300

    solved 3/4 exercises from conditionals/lists module

--- conditionals-and-lists/desafio-01/index.js
+++ conditionals-and-lists/desafio-01/index.js
@@ -1,3 +1,22 @@
 new Vue({
-
+  el: '#desafio',
+  data: {
+    alternarParagrafo: true,
+    livro: {
+      titulo: 'O senhor dos anéis',
+      autor: 'J.R.R. Tolkiens',
+      volume: '3'
+    },
+    aluno: {
+      id: 10,
+      nome: 'Maria',
+      notas: [7.67, 8.33, 6.98, 9.21]
+    },
+    brands: ['nike', 'adidas', 'puma', 'new balance']
+  },
+  methods: {
+    alternar() {
+      this.alternarParagrafo = !this.alternarParagrafo
+    }
+  }
 })
