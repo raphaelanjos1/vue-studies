1193a565c022a07e3921dc4543acfdcf9c0f08d3
raphaelanjos1 <raphael-anjos@live.com>
2020-02-04 17:09:54 -0300

    component communication

---
+++ components/components-communication/communication/src/components/UsuarioInfo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário: <strong>{{ reverseName() }}</strong></p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'name'
+  ],
+  methods: {
+    reverseName() {
+      return this.name.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
