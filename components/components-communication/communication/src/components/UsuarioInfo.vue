eb196ca350a20716f6f8db8ae7d5f6949302119b
raphaelanjos1 <raphael-anjos@live.com>
2020-05-04 14:32:01 -0300

    passagem de props

--- components/components-communication/communication/src/components/UsuarioInfo.vue
+++ components/components-communication/communication/src/components/UsuarioInfo.vue
@@ -1,28 +1,35 @@
 <template>
-    <div class="componente">
-        <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p>
-        <p>Nome do usuário: <strong>{{ reverseName() }}</strong></p>
-    </div>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do usuário:
+      <strong>{{ reverseName() }}</strong>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  props: [
-    'name'
-  ],
+  props: {
+    name: String,
+    required: true
+  },
   methods: {
     reverseName() {
-      return this.name.split('').reverse().join('')
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
     }
   }
-}
+};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
